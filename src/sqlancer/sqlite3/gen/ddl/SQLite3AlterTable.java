bc9555c0425e8016b0753db9f6717f21a31bb8c4
Jonathan Foo <jonathanfoo0523@gmail.com>
2024-06-07 14:35:30 +0000

    Fix: more specific error message

--- src/sqlancer/sqlite3/gen/ddl/SQLite3AlterTable.java
+++ src/sqlancer/sqlite3/gen/ddl/SQLite3AlterTable.java
@@ -49,7 +49,7 @@ public class SQLite3AlterTable {
         errors.add("CHECK constraint failed");
         errors.add("non-deterministic functions prohibited in index expressions");
         errors.add("non-deterministic functions prohibited in partial index WHERE clauses");
-        errors.add("[SQLITE_ERROR] SQL error or missing database (SQL logic error)");
+        errors.add("SQL logic error");
 
         Option option = Randomly.fromOptions(Option.values());
         SQLite3Table t = s.getRandomTableOrBailout(tab -> !tab.isView() && !tab.isVirtual() && !tab.isReadOnly());
